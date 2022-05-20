ef8376d3c303ade41c9433ddee092c5475223cd8
Doomfang3 <saubinmathieu@gmail.com>
2022-05-20 11:48:25 +0200

    Replace Handlebars with EJS

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -1,82 +1,82 @@
-const router = require("express").Router();
+const router = require('express').Router()
 
-const Book = require("../models/Book.model.js"); // <== add this line before your routes
+const Book = require('../models/Book.model.js') // <== add this line before your routes
 
 // GET route to display the form
-router.get("/books/create", (req, res) => res.render("books/book-create.hbs"));
+router.get('/books/create', (req, res) => res.render('books/book-create.ejs'))
 
 // POST route to save a new book to the database in the books collection
-router.post("/books/create", (req, res, next) => {
+router.post('/books/create', (req, res, next) => {
   // console.log(req.body);
-  const { title, author, description, rating } = req.body;
+  const { title, author, description, rating } = req.body
 
   Book.create({ title, author, description, rating })
     // .then(bookFromDB => console.log(`New book created: ${bookFromDB.title}.`))
-    .then(() => res.redirect("/books"))
-    .catch((error) => next(error));
-});
+    .then(() => res.redirect('/books'))
+    .catch(error => next(error))
+})
 
 // GET route to display the form to update a specific book
-router.get("/books/:bookId/edit", (req, res, next) => {
-  const { bookId } = req.params;
+router.get('/books/:bookId/edit', (req, res, next) => {
+  const { bookId } = req.params
 
   Book.findById(bookId)
-    .then((bookToEdit) => {
+    .then(bookToEdit => {
       // console.log(bookToEdit);
-      res.render("books/book-edit.hbs", { book: bookToEdit }); // <-- add this line
+      res.render('books/book-edit.ejs', { book: bookToEdit }) // <-- add this line
     })
-    .catch((error) => next(error));
-});
+    .catch(error => next(error))
+})
 
 // POST route to actually make updates on a specific book
-router.post("/books/:bookId/edit", (req, res, next) => {
-  const { bookId } = req.params;
-  const { title, description, author, rating } = req.body;
+router.post('/books/:bookId/edit', (req, res, next) => {
+  const { bookId } = req.params
+  const { title, description, author, rating } = req.body
 
   Book.findByIdAndUpdate(bookId, { title, description, author, rating }, { new: true })
-    .then((updatedBook) => res.redirect(`/books/${updatedBook.id}`)) // go to the details page to see the updates
-    .catch((error) => next(error));
-});
+    .then(updatedBook => res.redirect(`/books/${updatedBook.id}`)) // go to the details page to see the updates
+    .catch(error => next(error))
+})
 
 // POST route to delete a book from the database
-router.post("/books/:bookId/delete", (req, res, next) => {
-  const { bookId } = req.params;
+router.post('/books/:bookId/delete', (req, res, next) => {
+  const { bookId } = req.params
 
   Book.findByIdAndDelete(bookId)
-    .then(() => res.redirect("/books"))
-    .catch((error) => next(error));
-});
+    .then(() => res.redirect('/books'))
+    .catch(error => next(error))
+})
 
 // GET route to retrieve and display all the books
-router.get("/books", (req, res, next) => {
+router.get('/books', (req, res, next) => {
   Book.find()
-    .then((allTheBooksFromDB) => {
+    .then(allTheBooksFromDB => {
       // -> allTheBooksFromDB is a placeholder, it can be any word
-      console.log("Retrieved books from DB:", allTheBooksFromDB);
+      console.log('Retrieved books from DB:', allTheBooksFromDB)
 
       // we call the render method after we obtain the books data from the database -> allTheBooksFromDB
-      res.render("books/books-list.hbs", { books: allTheBooksFromDB }); // pass `allTheBooksFromDB` to the view (as a variable books to be used in the HBS)
+      res.render('books/books-list.ejs', { books: allTheBooksFromDB }) // pass `allTheBooksFromDB` to the view (as a variable books to be used in the EJS)
     })
-    .catch((error) => {
-      console.log("Error while getting the books from the DB: ", error);
+    .catch(error => {
+      console.log('Error while getting the books from the DB: ', error)
 
       // Call the error-middleware to display the error page to the user
-      next(error);
-    });
-});
+      next(error)
+    })
+})
 
 // GET route to retrieve and display details of a specific book
-router.get("/books/:bookId", (req, res, next) => {
-  const { bookId } = req.params;
+router.get('/books/:bookId', (req, res, next) => {
+  const { bookId } = req.params
 
   Book.findById(bookId)
-    .then((theBook) => res.render("books/book-details.hbs", { book: theBook }))
-    .catch((error) => {
-      console.log("Error while retrieving book details: ", error);
+    .then(theBook => res.render('books/book-details.ejs', { book: theBook }))
+    .catch(error => {
+      console.log('Error while retrieving book details: ', error)
 
       // Call the error-middleware to display the error page to the user
-      next(error);
-    });
-});
+      next(error)
+    })
+})
 
-module.exports = router;
+module.exports = router
